3d5ea6d45dcc7df64d9972e8853b42147b1c006a
James Solomon <solomonjames@spotify.com>
2014-10-10 14:31:54 -0400

    new app

--- playlocal/app.rb
+++ playlocal/app.rb
@@ -12,8 +12,8 @@ require 'json'
 get '/player/csrf' do
   response = open('https://playlister.spotilocal.com:4371/simplecsrf/token.json', 'Origin' => 'https://embed.spotify.com').read
 
-  headers \
-    "Access-Control-Allow-Origin" => "http://playlister.spotlet.io"
+  # headers \
+    # "Access-Control-Allow-Origin" => "http://playlister.spotlet.io"
 
   json :data => JSON.parse(response)
 end
@@ -21,8 +21,8 @@ end
 get '/player/token' do
   response = open('http://open.spotify.com/token', 'Origin' => 'https://embed.spotify.com').read
 
-  headers \
-    "Access-Control-Allow-Origin" => "http://playlister.spotlet.io"
+  # headers \
+    # "Access-Control-Allow-Origin" => "http://playlister.spotlet.io"
 
   json :data => JSON.parse(response)
 end
